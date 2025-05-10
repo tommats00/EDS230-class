9904fd33669bc325d3238ca636377314c56352b8
tommats00 <tbg@bren.ucsb.edu>
2025-05-10 15:50:25 -0700

    forest growth assignment, worked through it but need to check

---
+++ R/forest_growth.R
@@ -0,0 +1,17 @@
+#' @param time time since start
+#' @param r early exponential growth rate
+#' @param C size of the forest in kg/C
+#' @param g linear growth rate in kg/year
+#' @param K carrying capacity in units kg carbon
+
+
+forest_growth <- function (time, C, parms){
+  
+  if (C < parms$thresh){
+    dC <- parms$r * C
+  } else {
+    dC <- parms$g * (1 - C / parms$K)
+  }
+  
+  return(list(dC))
+}
